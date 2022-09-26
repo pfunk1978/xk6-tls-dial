7f09895d9246d1cd6dfe7ac5ba24b41612eaea7e
pfunk1978 <send.paulmoore.email@gmail.com>
2022-09-26 11:38:36 -0500

    Initial commit (#1)
    * First commit
    
    * Delete initial_tls_test.js
    
    * Updated Readme
    
    Co-authored-by: pmoore <pmoore@futurex.com>

--- initial_tls_test.js
+++ initial_tls_test.js
@@ -1,23 +1,16 @@
-# xk6-tls-dial
-
-A tls-wrapped, serialized, tcp api handler with mTLS support.
-This plugin only handles tls-wrapped sockets. For clear sockets use the TCP plugin instead.
-
-Usage:
-``` javaScript
 import tlsdial from 'k6/x/tlsdial';
 import { check } from 'k6';
 import { Counter } from 'k6/metrics';
 import { SharedArray } from 'k6/data';
 
-export const socketDataSent = new Counter('tls_data_sent');
-export const socketDataRecv = new Counter('tls_data_received');
+export const epDataSent = new Counter('tls_data_sent');
+export const epDataRecv = new Counter('tls_data_received');
 
-// Use a shared array to keep memory down
+
 const conf = new SharedArray('conf', function(){
     let c = Array()
     c[0] = {
-        insecure_skip_verify: true, // will validate on SAN, not utilized in this example
+        insecure_skip_verify: true,
         ca_certificates: [
             open('./pki/root.pem'),
             open('./pki/inter.pem'),
@@ -26,7 +19,6 @@ const conf = new SharedArray('conf', function(){
         client_certificate: open('./pki/client.pem'),
         client_key: open('./pki/client.skey')
     }
-    // add another object as an element if more than one config is to be used
     return c
 });
 
@@ -36,11 +28,10 @@ export default function () {
     // let conn = tlsdial.dial('10.30.16.22:2001', conf[0]); // connection for every VU to test handshake load
     let message = 'Say Hello';
     tlsdial.writeLn(conn, message);
-    socketDataSent.add(message.length);
+    epDataSent.add(message.length);
     let recv = tlsdial.readstring(conn);
-    socketDataRecv.add(recv.length);
+    epDataRecv.add(recv.length);
     check(recv, {
         'said Hello?': (recv) => recv.includes('Hello')
     });
 }
-```
